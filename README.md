c4876915385207fc13577de4bf6a2d2876efdf28
imba97 <mail@imba97.cn>
2022-01-14 23:26:17 +0800

    feat: 作为上传器使用
    开放更多的接口，作为上传器使用

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # simple-sftp-uploader
 
-一个简单的SFTP上传器，把指定文件夹中的文件上传到服务器上。
+一个简单的 SFTP 上传器，把指定文件夹中的文件上传到服务器上。
 
 可以单独用，也是个`webpack`插件。
 
 # 导入
 
-`import SftpUploader from 'simple-sftp-uploader'`
+```javascript
+import SftpUploader from 'simple-sftp-uploader'
+```
 
 # 配置
 
@@ -22,15 +24,14 @@ const sftpUploaderConfig = {
     username: 'root',
     privateKey: fs.readFileSync('C:/Users/imba97/.ssh/id_rsa')
   },
-  rmExclude: [
-    'favicon.ico'
-  ]
+  rmExclude: ['favicon.ico']
 }
 ```
 
 # 上传
 
 ## 普通上传
+
 ```javascript
 import SftpUploader from 'simple-sftp-uploader'
 
@@ -46,9 +47,7 @@ import SftpUploader from 'simple-sftp-uploader'
 
 // ...
 {
-  plugins: [
-    new SftpUploader(sftpUploaderConfig)
-  ]
+  plugins: [new SftpUploader(sftpUploaderConfig)]
 }
 ```
 
@@ -69,4 +68,47 @@ import SftpUploader from 'simple-sftp-uploader'
   },
   // ...
 }
+```
+
+## 作为上传器使用
+
+配置连接信息
+
+```javascript
+const uploader = new Uploader({
+  connect: {
+    host: '1.2.3.4',
+    port: 22,
+    username: 'root',
+    privateKey: fs.readFileSync('C:/Users/imba97/.ssh/id_rsa')
+  }
+})
+```
+
+开放的接口
+
+```typescript
+// 连接，连接后才能执行以下操作
+connect(): Promise<null>
+
+// 上传文件
+uploadFile(local: string, remote: string): Promise<null>
+
+// 判断远程文件是否存在
+exists(src: string): Promise<boolean>
+
+// 删除文件夹内所有文件
+deleteFiles(remote: string, exclude?: RegExp): Promise<null>
+
+// 读取文件夹下的文件
+readdir(src: string): Promise<string[]>
+
+// 执行 shell 命令
+exec(script: string): Promise<null>
+
+// 创建文件夹
+mkdir(dirPath: string)
+
+// 关闭
+close(): void
 ```
--- src/SftpUploader.ts
+++ src/SftpUploader.ts
@@ -3,13 +3,12 @@
  * 可以指定一个目录，将里面的文件上传到远程服务器的指定目录中
  */
 
-import { Client as SSHClient, SFTPWrapper, ConnectConfig, ClientErrorExtensions } from 'ssh2'
+import { Client as SSHClient, SFTPWrapper, ConnectConfig } from 'ssh2'
 import { generateDirectoryTree } from './utils'
 
 import path from 'path'
 
 export default class SftpUploadPlugin {
-
   private _client!: SSHClient
   private _sftp: SFTPWrapper
 
@@ -27,9 +26,9 @@ export default class SftpUploadPlugin {
     local: string[]
     remote: string[]
   } = {
-      local: [],
-      remote: []
-    }
+    local: [],
+    remote: []
+  }
 
   /**
    * 远程文件夹中的文件列表
@@ -37,38 +36,42 @@ export default class SftpUploadPlugin {
   private _remoteDirFiles: string[]
 
   private _consoleMark = {
-    start: '┌',
-    info: '├',
-    end: '└'
+    start: '',
+    info: '',
+    end: ''
   }
 
   constructor(options: SftpUploaderOptions) {
-
     // 配置
     this._local = options.localDir
     this._remote = options.remoteDir
     this._rmExclude = options.rmExclude || []
     this._connect = options.connect
-
   }
 
   // Webpack 钩子
   apply(compiler: any) {
     // 打包完成后
-    compiler.hooks.done.tapAsync('SftpUploadPlugin', async (compilation: any, callback: () => any) => {
-      callback()
-      // production 才会触发插件
-      if (compiler.options.mode === 'production') {
-        console.log(`[SftpUploadPlugin]: Upload to remote ${this._remote}`)
-        await this.start()
+    compiler.hooks.done.tapAsync(
+      'SftpUploadPlugin',
+      async (compilation: any, callback: () => any) => {
+        callback()
+        // production 才会触发插件
+        if (compiler.options.mode === 'production') {
+          console.log(`[SftpUploadPlugin]: Upload to remote ${this._remote}`)
+          await this.start()
+        }
       }
-    })
+    )
   }
 
   /**
    * 执行上传
    */
   public async start() {
+    this._consoleMark.start = '┌'
+    this._consoleMark.info = '├'
+    this._consoleMark.end = '└'
 
     // 目录树
     this._directoryTree = generateDirectoryTree(this._local, this._remote)
@@ -81,7 +84,7 @@ export default class SftpUploadPlugin {
       // 获取文件夹内容
       this._remoteDirFiles = await this.readdir(this._remote)
       // 清理
-      await this.cleardir()
+      await this.doCleardir()
       console.log(`${this._consoleMark.end} 清空完成`)
     }
 
@@ -93,12 +96,39 @@ export default class SftpUploadPlugin {
   }
 
   /**
+   * 连接
+   */
+  public connect(): Promise<null> {
+    return new Promise((resolve, reject) => {
+      // 初始化 SSH 客户端
+      this._client = new SSHClient()
+      this._client
+        .on('ready', () => {
+          this._client.sftp((_err, _sftp) => {
+            this._sftp = _sftp
+            resolve(null)
+          })
+        })
+        .on('error', (err) => {
+          reject(err)
+        })
+        .connect(this._connect)
+    })
+  }
+
+  public close() {
+    this._sftp.end()
+    this._client.end()
+  }
+
+  /**
    * 上传文件
    * @param _local 本地路径
    * @param _remote 远程路径
    * @return
    */
-  public async uploadFile(_local: string, _remote: string) {
+  public async uploadFile(_local: string, _remote: string): Promise<null> {
+    await this.mkdir(_remote)
     return await this.doUpload(_local, _remote)
   }
 
@@ -107,37 +137,37 @@ export default class SftpUploadPlugin {
    * @param {string} src
    * @return {Promise<boolean>} 是否存在
    */
-  public exists(src: string) {
+  public exists(src: string): Promise<boolean> {
     return new Promise((resolve) => {
-      this._sftp.exists(src, isExists => {
+      this._sftp.exists(src, (isExists) => {
         resolve(isExists)
       })
     })
   }
 
   /**
-   * 清空远程目录下文件
-   * @param i 当前清空下标
-   * @return
+   * 清空文件夹
+   * @param remote 远程地址
+   * @param exclude 排除文件名
+   * @returns
    */
-  public async cleardir(i = 0): Promise<null> {
-    if (this._remoteDirFiles.length === 0) return Promise.resolve(null)
+  public async deleteFiles(remote: string, exclude?: RegExp): Promise<null> {
+    this._remote = remote
 
-    const filename = this._remoteDirFiles[i]
-    // 不是排除文件
-    if (this._rmExclude.indexOf(filename) === -1) {
-      console.log(`${this._consoleMark.info} 删除 ${filename}`)
-      await this.exec(`rm -rf ${this._remote}/${filename}`)
-    } else {
-      console.log(`${this._consoleMark.info} 排除 ${filename}`)
-    }
-
-    // 遍历完后返回
-    if (++i === this._remoteDirFiles.length) {
+    if (!(await this.exists(remote))) {
       return Promise.resolve(null)
-    } else {
-      return this.cleardir(i)
     }
+
+    // 获取文件夹内容
+    const files = await this.readdir(remote)
+
+    // 排除
+    this._remoteDirFiles = exclude
+      ? files.filter((filename) => !exclude.test(filename))
+      : files
+
+    // 清理
+    return this.doCleardir()
   }
 
   /**
@@ -152,18 +182,14 @@ export default class SftpUploadPlugin {
           .on('close', () => {
             resolve(null)
           })
-          .on(
-            'data',
-            (data: any) => {
-              stream.end()
-              if (data) resolve(null)
-            })
-          .stderr.on(
-            'data',
-            (data) => {
-              stream.end()
-              if (data) resolve(null)
-            })
+          .on('data', (data: any) => {
+            stream.end()
+            if (data) resolve(null)
+          })
+          .stderr.on('data', (data) => {
+            stream.end()
+            if (data) resolve(null)
+          })
       })
     })
   }
@@ -172,10 +198,14 @@ export default class SftpUploadPlugin {
    * 读取远程文件夹下文件
    * @param src 远程路径
    */
-  public readdir(src: string): Promise<string[]> {
+  public async readdir(src: string): Promise<string[]> {
+    if (!(await this.exists(src))) {
+      return Promise.resolve([])
+    }
+
     return new Promise((resolve) => {
       this._sftp.readdir(src, (_readdirErr, handle) => {
-        resolve(handle.map(item => item.filename))
+        resolve(handle.map((item) => item.filename))
       })
     })
   }
@@ -183,13 +213,22 @@ export default class SftpUploadPlugin {
   /**
    * 创建文件夹
    * @param dirPath 文件夹路径
-   * @param index 嵌套文件夹的 当前 index
-   * @return 
+   * @return
    */
-  public async mkdir(dirPath: string | string[], index = 0) {
+  public async mkdir(dirPath: string): Promise<null> {
+    return this.doMkdir(dirPath)
+  }
+
+  /**
+   * 创建文件夹
+   * @param dirPath 文件夹路径
+   * @param index 嵌套文件夹的 当前 index
+   * @return
+   */
+  private async doMkdir(dirPath: string | string[], index = 0): Promise<null> {
     return new Promise((resolve) => {
-      (async () => {
-        if (typeof dirPath === 'string' && await this.exists(dirPath)) {
+      ;(async () => {
+        if (typeof dirPath === 'string' && (await this.exists(dirPath))) {
           resolve(null)
           return
         }
@@ -202,7 +241,7 @@ export default class SftpUploadPlugin {
           .map((p, _index) => (_index <= index ? `/${p}` : ''))
           .join('')}`
 
-        if (!await this.exists(fullPath)) {
+        if (!(await this.exists(fullPath))) {
           this._sftp.mkdir(
             fullPath,
             {
@@ -211,7 +250,7 @@ export default class SftpUploadPlugin {
             () => {
               // 下一级目录
               if (index < dirs.length - 1) {
-                resolve(this.mkdir(dirs, index + 1))
+                resolve(this.doMkdir(dirs, index + 1))
               } else {
                 resolve(null)
               }
@@ -220,7 +259,7 @@ export default class SftpUploadPlugin {
         } else {
           // 下一级目录
           if (index < dirs.length - 1) {
-            resolve(this.mkdir(dirs, index + 1))
+            resolve(this.doMkdir(dirs, index + 1))
           } else {
             resolve(null)
           }
@@ -237,40 +276,16 @@ export default class SftpUploadPlugin {
   }
 
   /**
-   * 连接
-   */
-  private connect(): Promise<null> {
-    return new Promise((resolve, reject) => {
-      // 初始化 SSH 客户端
-      this._client = new SSHClient()
-      this._client
-        .on('ready', () => {
-
-          this._client.sftp((_err, _sftp) => {
-            this._sftp = _sftp
-            resolve(null)
-          })
-        })
-        .on('error', (err) => {
-          reject(err)
-        })
-        .connect(this._connect)
-    })
-  }
-
-  private close() {
-    this._sftp.end()
-    this._client.end()
-  }
-
-  /**
    * 上传文件
    * @param i 当前遍历下标
    */
   private async upload(i = 0): Promise<any> {
     const path = this._directoryTree.remote[i]
     await this.mkdir(path)
-    await this.doUpload(this._directoryTree.local[i], this._directoryTree.remote[i])
+    await this.doUpload(
+      this._directoryTree.local[i],
+      this._directoryTree.remote[i]
+    )
     if (++this._uploadFinshed === this._directoryTree.remote.length) {
       console.log(`${this._consoleMark.end} 上传完成`)
       this._client.end()
@@ -285,7 +300,7 @@ export default class SftpUploadPlugin {
    * @param _remote 远程路径
    * @return
    */
-  private async doUpload(_local: string, _remote: string) {
+  private async doUpload(_local: string, _remote: string): Promise<null> {
     return new Promise((resolve) => {
       this._sftp.fastPut(_local, _remote, (err) => {
         if (err) {
@@ -298,17 +313,41 @@ export default class SftpUploadPlugin {
     })
   }
 
+  /**
+   * 清空远程目录下文件
+   * @param i 当前清空下标
+   * @return
+   */
+  private async doCleardir(i = 0): Promise<null> {
+    if (this._remoteDirFiles.length === 0) return Promise.resolve(null)
+
+    const filename = this._remoteDirFiles[i]
+    // 不是排除文件
+    if (this._rmExclude.indexOf(filename) === -1) {
+      console.log(`${this._consoleMark.info} 删除 ${filename}`)
+      await this.exec(`rm -rf ${this._remote}/${filename}`)
+    } else {
+      console.log(`${this._consoleMark.info} 排除 ${filename}`)
+    }
+
+    // 遍历完后返回
+    if (++i === this._remoteDirFiles.length) {
+      return Promise.resolve(null)
+    } else {
+      return this.doCleardir(i)
+    }
+  }
 }
 
 interface SftpUploaderOptions {
   /**
-  * 本地目录
-  */
+   * 本地目录
+   */
   localDir: string
 
   /**
-  * 远程目录
-  */
+   * 远程目录
+   */
   remoteDir: string
 
   /**
@@ -317,7 +356,7 @@ interface SftpUploaderOptions {
   connect: ConnectConfig
 
   /**
-  * 删除远程文件时要忽略的目录
-  */
+   * 删除远程文件时要忽略的目录
+   */
   rmExclude?: string[]
 }
